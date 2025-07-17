4a9d8a528825067aecbbfc7558fe241a2d41af77
Thales Oliveira <TOliveiraDev@outlook.com>
2025-07-16 21:40:52 -0300

    Organizar rotas de estabelecimentos

--- src/routes/route.estabelecimento.js
+++ src/routes/route.estabelecimento.js
@@ -4,7 +4,10 @@ import { verifyJWT } from "../config/token.js";
 
 const routeEstabelecimento = Router();
 
+// Destaques
 routeEstabelecimento.get("/v1/estabelecimentos/destaques", verifyJWT, controllerEstabelecimento.GetDestaques);
+
+// Favoritos
 routeEstabelecimento.get("/v1/estabelecimentos/favoritos", verifyJWT, controllerEstabelecimento.GetFavoritos);
 routeEstabelecimento.post("/v1/estabelecimentos/favoritos", verifyJWT, controllerEstabelecimento.SetFavorito);
 routeEstabelecimento.delete("/v1/estabelecimentos/favoritos/:id_favorito", verifyJWT, controllerEstabelecimento.DeleteFavorito);
