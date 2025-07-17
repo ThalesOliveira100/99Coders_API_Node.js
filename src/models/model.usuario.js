f1331b5c501429fcef439ac24d3589ec052646bd
Thales Oliveira <TOliveiraDev@outlook.com>
2025-07-16 22:20:56 -0300

    Added User's address model and route in usuario's model / route.

--- src/models/model.usuario.js
+++ src/models/model.usuario.js
@@ -79,6 +79,139 @@ const EditUserByID = (id_usuario, nome, email, callback) => {
         callback(err, result);
     });
 }
-  
 
-export default {Login, Register, GetUserById, EditUserByID};
+const GetEnderecos = (id_usuario, id_endereco, cod_cidade, callback) => {
+    let filtro = [];
+
+    let ssql = `
+        SELECT id_endereco, id_usuario, endereco, complemento, bairro, cidade, uf, cep, ind_padrao, cod_cidade 
+        FROM tab_usuario_endereco 
+        WHERE id_usuario = ? 
+    `;
+
+    filtro.push(id_usuario);
+
+    if(id_endereco) {
+        ssql += "and id_endereco = ? "
+        filtro.push(id_endereco);
+    }
+
+    if(cod_cidade) {
+        ssql += "and cod_cidade = ? "
+        filtro.push(cod_cidade);
+    }
+
+    ssql += "order by id_endereco desc ";
+
+    db.query(ssql, filtro, function(err, result){
+        callback(err, result);
+    });
+}
+
+const SetEndereco = (id_usuario, dadosEnd, callback) => {
+    let ssql = `
+        INSERT INTO tab_usuario_endereco(id_usuario, endereco, complemento, bairro, cidade, uf, cep, ind_padrao, cod_cidade) 
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+        ssql, 
+        [
+            id_usuario, 
+            dadosEnd.endereco, 
+            dadosEnd.complemento, 
+            dadosEnd.bairro, 
+            dadosEnd.cidade, 
+            dadosEnd.uf, 
+            dadosEnd.cep, 
+            dadosEnd.ind_padrao, 
+            dadosEnd.cod_cidade
+        ], 
+        function(err, result){
+            callback(err, result);
+    });
+}
+
+const AlterEndereco = (id_usuario, id_endereco, dadosEnd, callback) => {
+    let ssql = `
+        UPDATE tab_usuario_endereco SET endereco=?, complemento=?, bairro=?, cidade=?, uf=?, cep=?, cod_cidade=?, 
+        WHERE id_endereco=? AND id_usuario=? 
+    `;
+
+    db.query(
+        ssql, 
+        [
+            id_usuario,
+            id_endereco,
+            dadosEnd.endereco, 
+            dadosEnd.complemento, 
+            dadosEnd.bairro, 
+            dadosEnd.cidade, 
+            dadosEnd.uf, 
+            dadosEnd.cep, 
+            dadosEnd.cod_cidade,
+            id_endereco,
+            id_usuario
+        ], 
+        function(err, result){
+            callback(err, result);
+    });
+}
+
+const DeleteEndereco = (id_endereco, id_usuario, callback) => {
+    let ssql = `
+        DELETE FROM tab_usuario_endereco 
+        WHERE id_endereco=? AND id_usuario=? 
+    `;
+
+    db.query(
+        ssql, [id_endereco, id_usuario], function(err, result){
+            callback(err, result);
+    });
+}
+
+const EnderecoPadrao = (id_endereco, id_usuario, callback) => {
+    db.getConnection((err, conn) => {
+
+        conn.beginTransaction(async (err) => {
+
+            let ssql = `
+                UPDATE tab_usuario_endereco 
+                SET ind_padrao = 'N' 
+                WHERE id_usuario=? 
+            `;
+            
+            ssql += "values(?, ?, ?, current_timestamp()) ";
+
+            conn.query(ssql, [id_usuario], (err, result) => {
+                
+                if(err) {
+                    conn.rollback();
+                    callback(err, result);
+                } else {
+
+                    ssql = `
+                        UPDATE tab_usuario_endereco 
+                        SET ind_padrao = 'S' 
+                        WHERE id_endereco=? AND id_usuario=? 
+                    `;
+
+                    conn.query(ssql, [id_endereco, id_usuario], (err, result) => {
+
+                        if(err){
+                            conn.rollback();
+                            callback(err, result)
+                        } else {
+                            conn.commit();
+                            callback(err, {id_usuario}); // {"id_usuario": 123}
+                        }
+
+                        conn.release();
+                    });
+                }
+            });
+        });
+    });
+}
+
+export default {Login, Register, GetUserById, EditUserByID, GetEnderecos, SetEndereco, AlterEndereco, DeleteEndereco, EnderecoPadrao};
