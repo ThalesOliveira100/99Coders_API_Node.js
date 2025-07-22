eda3e310ac9d6e5bc778c303628445fb2afefdfb
Thales Oliveira <toliveiradev@outlook.com>
2025-07-22 10:13:25 -0300

    added user'a controller

--- src/controllers/controller.usuario.js
+++ src/controllers/controller.usuario.js
@@ -63,4 +63,65 @@ const EditUserByID = (req, res) => {
     });
 }
 
-export default {Login, Register, GetUserById, EditUserByID};
+const GetEnderecos = (req, res) => {
+
+    modelUsuario.GetEnderecos(req.id_usuario, req.params.id_endereco, req.query.cod_cidade, function (err, result) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json(result);
+        };
+    });
+}
+
+const SetEndereco = (req, res) => {
+    modelUsuario.SetEndereco(req.id_usuario, req.body, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).json({id_endereco: result.insertId});
+        };
+    })
+}
+
+const AlterEndereco = (req, res) => {
+    modelUsuario.AlterEndereco(req.id_usuario, req.params.id_endereco, req.body, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json({id_endereco: req.params.id_endereco});
+        };
+    })
+}
+
+const DeleteEndereco = (req, res) => {
+    modelUsuario.DeleteEndereco(req.params.id_endereco, req.id_usuario, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json({id_endereco: req.params.id_endereco});
+        };
+    })
+}
+
+const EnderecoPadrao = (req, res) => {
+    modelUsuario.EnderecoPadrao(req.params.id_endereco, req.id_usuario, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json({id_endereco: req.params.id_endereco});
+        };
+    })
+}
+
+export default {
+    Login, 
+    Register, 
+    GetUserById, 
+    EditUserByID, 
+    GetEnderecos, 
+    SetEndereco, 
+    AlterEndereco, 
+    DeleteEndereco, 
+    EnderecoPadrao
+};
