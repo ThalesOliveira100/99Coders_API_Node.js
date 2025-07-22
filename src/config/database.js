8e5c5debfd2bd8d91c4de9ac91672378ef45eb27
Thales Oliveira <TOliveiraDev@outlook.com>
2025-07-22 18:43:09 -0300

    added executeQuery async func

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,19 @@ const db = mysql.createPool({
     user: "root",
     password: "12345",
     database: "delivery_mais__99node",
-})
+});
+
+async function executeQuery(connection, query, parameters) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, parameters, (err, result) => {
+            if (err) {
+                return reject(err);
+            } else {
+                return resolve(result);
+            }
+        })
+    })
+}
 
 db.getConnection((err, conn) => {
     if (err) {
